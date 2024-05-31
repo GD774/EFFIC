9e1dc2315eda36625436410d18d68e1f4dacb958
DESKTOP-5N7EHG6\js <greybrown4645@gmail.com>
2024-06-01 01:13:17 +0900

    사이드바 링크 설정 및 아이콘 변경

--- src/main/java/com/gd774/effic/controller/MsgController.java
+++ src/main/java/com/gd774/effic/controller/MsgController.java
@@ -4,13 +4,38 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping(value="msg")
+@RequestMapping(value="/msg")
 @Controller
 public class MsgController {
 	
-	@GetMapping(value="inboxList")
+	@GetMapping(value="/write.page")
+	public String goWrite() {
+		return "msg/write";
+	}
+	
+	@GetMapping(value="/toMe.page")
+	public String goToMe() {
+		return "msg/toMe";
+	}
+	
+	@GetMapping(value="/inboxList.page")
 	public String goInboxList() {
 		return "msg/inboxList";
+	}
+	
+	@GetMapping(value="/sentList.page")
+	public String goSentList() {
+		return "msg/sentList";
+	}
+	
+	@GetMapping(value="/impList.page")
+	public String goImpList() {
+		return "msg/impList";
+	}
+	
+	@GetMapping(value="/bin.page")
+	public String goBin() {
+		return "msg/bin";
 	}
 	
 	

5f9dc4eb83f8fdc7ad03f4695c0e6563e532ef4b
greyBrown <greybrown4645@gmail.com>
2024-06-03 09:13:16 +0900

    merge 준비

--- src/main/java/com/gd774/effic/dto/MsgAttachDto.java
+++
@@ -1,5 +0,0 @@
-package com.gd774.effic.dto;
-
-public class MsgAttachDto {
-	String attachId, msgId, uploadPath, filesysName, originalName;
-}
--- src/main/java/com/gd774/effic/dto/RecpDto.java
+++
@@ -1,9 +0,0 @@
-package com.gd774.effic.dto;
-
-import java.sql.Date;
-
-public class RecpDto {
-	String recpId, msgId, recipient;
-	Date readDt;
-	int state, chkImpt;
-}

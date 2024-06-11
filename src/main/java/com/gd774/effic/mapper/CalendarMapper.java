77282cf7a73b66e54d173ad50e79108c6c600ba1
swko630 <swko630>
2024-06-11 13:44:38 +0900

    중간결산

--- src/main/java/com/gd774/effic/mapper/CalendarMapper.java
+++ src/main/java/com/gd774/effic/mapper/CalendarMapper.java
@@ -2,6 +2,7 @@ package com.gd774.effic.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.gd774.effic.dto.CalendarDto;
@@ -9,6 +10,7 @@ import com.gd774.effic.dto.CalendarDto;
 @Mapper
 public interface CalendarMapper {
     List<CalendarDto> selectAllSchedules();
+    @Insert("INSERT INTO schedule (title, start_dt, end_dt, contents, doc_state, all_day) VALUES (#{title}, #{startDt}, #{endDt}, #{contents}, #{docState}, #{allDay})")
     int insertSchedule(CalendarDto calendarDto);
     int updateSchedule(CalendarDto calendarDto);
     int deleteSchedule(int scheduleId);
